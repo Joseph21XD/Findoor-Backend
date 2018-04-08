6ae378d3ea8b9f9b9bd1a731c9535feccf40e371
Joseph <ramirezjantonio@hotmail.com>
2018-04-08 14:39:03 -0600

    arreglo de parser de datos

--- hello/views.py
+++ hello/views.py
@@ -6,6 +6,17 @@ import string
 import random
 
 
+def lista_persona(lista):
+		personas= "{ personas : ["
+		for i in range(len(lista)):
+			if(i!=len(lista)-1):
+				personas+="{id : "+parsear(str(lista[i].id))+" , nombre : "+parsear(lista[i].nombre)+" , apellido : "+parsear(lista[i].apellido)+" , imagen : "+parsear(lista[i].imagen)+" ,"
+			else:
+				personas+="{id : "+parsear(str(lista[i].id))+" , nombre : "+parsear(lista[i].nombre)+" , apellido : "+parsear(lista[i].apellido)+" , imagen : "+parsear(lista[i].imagen)+" }"
+		personas+="], token :"+validate[0].token+" }"
+		return personas
+	
+
 def personJson(request, tok):
 	validate= Persona.objects.filter(token=tok)	
 	if(len(validate)>0):
@@ -14,14 +25,7 @@ def personJson(request, tok):
 		validate[0].save()
 		if(len(lista)==0):
 			return HttpResponse("")
-		personas= "{ personas : ["
-		for i in range(len(lista)):
-			if(i!=len(lista)-1):
-				personas+="{id : "+str(lista[i].id)+" , nombre : "+lista[i].nombre+" , apellido : "+lista[i].apellido+" , imagen : "+lista[i].imagen+" ,"
-			else:
-				personas+="{id : "+str(lista[i].id)+" , nombre : "+lista[i].nombre+" , apellido : "+lista[i].apellido+" , imagen : "+lista[i].imagen+" }"
-		personas+="], token :"+validate[0].token+" }"
-		return HttpResponse(personas)
+		return HttpResponse(lista_persona(lista))
 	else:
 		raise Http404
 
@@ -34,21 +38,19 @@ def person_id_Json(request, id_personaje, tok):
 		validate[0].save()
 		if(len(lista)==0):
 			return HttpResponse("")
-		persona= ""
-		for i in range(len(lista)):
-			if(i!=len(lista)-1):
-				persona+="{ id : "+str(lista[i].id)+" , nombre  : "+lista[i].nombre+" , apellido : "+lista[i].apellido+" , imagen : "+lista[i].imagen+" , token : "+tok+" },"
-			else:
-				persona+="{ id : "+str(lista[i].id)+" , nombre : "+lista[i].nombre+" , apellido : "+lista[i].apellido+" , imagen : "+lista[i].imagen+" , token : "+tok+" }"
-		return HttpResponse(persona)
+		return HttpResponse(lista_persona(lista))
 	else:
 		raise Http404
 	
 def person_add(request, name, lastName, isFace, mail, pwd, img):
+	name= deparsear(name)
+	lastname= deparsear(lastName)
+	mail= deparsear(mail)
+	pwd= deparsear(pwd)
+	img= deparsear(pwd)
 	i= len(Persona.objects.all())
 	tok=getToken()
-	mail= parseURL(mail)
-	p= Persona(nombre= name, apellido= lastName,  isFacebook= isFace, correo=mail , contrasenna= pwd, imagen= parseURL(img), token= tok)
+	p= Persona(nombre= name, apellido= lastName,  isFacebook= isFace, correo=mail , contrasenna= pwd, imagen= img, token= tok)
 	p.save()
 	k= len(Persona.objects.all())
 	if( k > i):
@@ -59,12 +61,12 @@ def person_add(request, name, lastName, isFace, mail, pwd, img):
 def person_update(request,id_persona, name, lastName, isFace,mail, pwd, img, tok):
 	validate= Persona.objects.filter(id=id_persona, token=tok)
 	if(len(validate)>0):
-		validate[0].nombre=name
-		validate[0].apellido=lastName
+		validate[0].nombre=deparsear(name)
+		validate[0].apellido=deparsear(lastName)
 		validate[0].isFacebook=isFace
-		validate[0].contrasenna=pwd
-		validate[0].correo= parseURL(mail)
-		validate[0].imagen= parseURL(img)
+		validate[0].contrasenna=deparsear(pwd)
+		validate[0].correo= deparsear(mail)
+		validate[0].imagen= deparsear(img)
 		k= getToken()
 		validate[0].token=k
 		validate[0].save()
@@ -78,18 +80,19 @@ def getToken():
 	return str1
 	
 def loginByCredentials(request, mail, pwd):
-		mail=parseURL(mail)
-		person= Persona.objects.filter(correo=mail, contrasenna= pwd)
-		if(len(person)>0):
-			persona="{ id : "+str(person[0].id)+" , nombre : "+person[0].nombre+" , apellido : "+person[0].apellido+" , imagen : "+person[0].imagen+" , correo : "+person[0].correo+" , contrasenna : "+person[0].contrasenna+" , token : "+person[0].token+"},"
-			return HttpResponse(persona)
-		else:
-			raise Http404
+	mail=deparsear(mail)
+	pwd=deparsear(pwd)
+	person= Persona.objects.filter(correo=mail, contrasenna= pwd)
+	if(len(person)>0):
+		persona="{ id : "+str(person[0].id)+" , nombre : "+parsear(person[0].nombre)+" , apellido : "+parsear(person[0].apellido)+" , imagen : "+parsear(person[0].imagen)+" , correo : "+parsear(person[0].correo)+" , contrasenna : "+parsear(person[0].contrasenna)+" , token : "+person[0].token+"}"
+		return HttpResponse(persona)
+	else:
+		raise Http404
 			
 def loginByToken(request, tok):
 	person= Persona.objects.filter(token=tok)
 	if(len(person)>0):
-		persona="{ id : "+str(person[0].id)+" , nombre : "+person[0].nombre+" , apellido : "+person[0].apellido+" , imagen : "+person[0].imagen+" , correo : "+person[0].correo+" , contrasenna : "+person[0].contrasenna+" , token : "+person[0].token+"},"
+		persona="{ id : "+str(person[0].id)+" , nombre : "+parsear(person[0].nombre)+" , apellido : "+parsear(person[0].apellido)+" , imagen : "+parsear(person[0].imagen)+" , correo : "+parsear(person[0].correo)+" , contrasenna : "+parsear(person[0].contrasenna)+" , token : "+person[0].token+"}"
 		return HttpResponse(persona)
 	else:
 		raise Http404
@@ -110,11 +113,11 @@ def sitioToJson(lista, token):
 	sites="{ sitios : ["
 	for i in range(len(lista)):
 			if(i!=len(lista)-1):
-				sites+="{ id : "+str(lista[i].id)+" , nombre : "+ deparse(lista[i].nombre) +" , latitud : "+lista[i].latitud+" , longuitud : "+lista[i].longuitud
-				sites+=" , imagen : "+lista[i].imagen+" , descripcion :"+ deparse(lista[i].descripcion) +" , direccion : "+deparse(lista[i].direccion)+" } , "
+				sites+="{ id : "+str(lista[i].id)+" , nombre : "+ parsear(lista[i].nombre) +" , latitud : "+parsear(lista[i].latitud)+" , longuitud : "+parsear(lista[i].longuitud)
+				sites+=" , imagen : "+parsear(lista[i].imagen)+" , descripcion :"+ parsear(lista[i].descripcion) +" , direccion : "+parsear(lista[i].direccion)+" } , "
 			else:
-				sites+="{id : "+str(lista[i].id)+" , nombre : "+ deparse(lista[i].nombre)+" , latitud : "+lista[i].latitud+" , longuitud : "+lista[i].longuitud
-				sites+=" , imagen : "+lista[i].imagen+" , descripcion : "+ deparse(lista[i].descripcion) +" , direccion : "+ deparse(lista[i].direccion)+" }"
+				sites+="{ id : "+str(lista[i].id)+" , nombre : "+ parsear(lista[i].nombre) +" , latitud : "+parsear(lista[i].latitud)+" , longuitud : "+parsear(lista[i].longuitud)
+				sites+=" , imagen : "+parsear(lista[i].imagen)+" , descripcion :"+ parsear(lista[i].descripcion) +" , direccion : "+parsear(lista[i].direccion)+" } "
 	sites+="], token : "+token+" }"
 	return sites
 	
@@ -151,9 +154,10 @@ def getVisited(id_persona, person):
 
 def sitio_comment(request, id_site, comment, tok):
 	validate= Persona.objects.filter(token=tok)
+	comment= deparsear(comment)
 	if(len(validate)>0):
 		site= Sitio.objects.filter(id= id_site)
-		comment= parseDesc(comment)
+		comment= parsear(comment)
 		comm= Comentario(persona= validate[0], sitio=site[0], comentario= comment)
 		comm.save()
 		k= getToken()
@@ -172,11 +176,11 @@ def sitio_comment_Json(request,id_site,tok):
 		comments="{ comentarios : ["
 		for i in range(len(lista)):
 			if(i!=len(lista)-1):
-				comments+="{ id : "+str(lista[i].id)+" , nombre : "+lista[i].persona.nombre+" , apellido : "+lista[i].persona.apellido+" , imagen : "+lista[i].persona.imagen
-				comments+=", comentario : "+lista[i].comentario+" } , "
+				comments+="{ id : "+str(lista[i].id)+" , nombre : "+parsear(lista[i].persona.nombre)+" , apellido : "+parsear(lista[i].persona.apellido)+" , imagen : "+parsear(lista[i].persona.imagen)
+				comments+=", comentario : "+parsear(lista[i].comentario)+" } , "
 			else:
-				comments+="{ id : "+str(lista[i].id)+" , nombre : "+lista[i].persona.nombre+" , apellido : "+lista[i].persona.apellido+" , imagen : "+lista[i].persona.imagen
-				comments+=", comentario : "+lista[i].comentario+" } "
+				comments+="{ id : "+str(lista[i].id)+" , nombre : "+parsear(lista[i].persona.nombre)+" , apellido : "+parsear(lista[i].persona.apellido)+" , imagen : "+parsear(lista[i].persona.imagen)
+				comments+=", comentario : "+parsear(lista[i].comentario)+" } "
 		comments+="] , token : "+token+" }"
 		validate[0].token= token
 		validate[0].save()
@@ -188,12 +192,12 @@ def sitio_suggest(request,nom,lat,lon,dir,desc,img,tok):
 	validate= Persona.objects.filter(token= tok)
 	if(len(validate)>0):
 		k= getToken()
-		desc= parseDesc(desc)
-		dir= parseDesc(dir)
-		img= parseURL(img)
-		lat= parseCoor(lat)
-		lon= parseCoor(lon)
-		nom= parseDesc(nom)
+		desc= deparsear(desc)
+		dir= deparsear(dir)
+		img= deparsear(img)
+		lat= deparsear(lat)
+		lon= deparsear(lon)
+		nom= deparsear(nom)
 		suggest= Recomendacion(nombre= nom, latitud=lat, longuitud=lon,direccion=dir,descripcion=desc,imagen=img, persona=validate[0])
 		suggest.save()
 		validate[0].token= k
@@ -240,15 +244,7 @@ def sitio_ranking_Json(request, tok):
 		validate[0].save()
 		lista=[]
 		lista= getRanking()
-		sites="{ sitios : ["
-		for i in range(len(lista)):
-			if(i!=len(lista)-1):
-				sites+="{ id : "+str(lista[i].id)+" , nombre : "+lista[i].nombre+" , latitud : "+lista[i].latitud+" , longuitud : "+lista[i].longuitud
-				sites+=", imagen "+lista[i].imagen+", descripcion"+lista[i].descripcion+", descripcion"+lista[i].descripcion+" } , "
-			else:
-				sites+="{id : "+str(lista[i].id)+" , nombre : "+lista[i].nombre+" , latitud : "+lista[i].latitud+" , longuitud : "+lista[i].longuitud
-				sites+=", imagen : "+lista[i].imagen+", descripcion : "+lista[i].descripcion+", direccion : "+lista[i].direccion+" }"
-		sites+="], token : "+tok+" }"
+		sites= sitioToJson(lista)
 		return HttpResponse(sites)
 	else:
 		raise Http404
@@ -256,8 +252,8 @@ def sitio_ranking_Json(request, tok):
 def sitio_close(request, lat , lon, tok):
 	validate= Persona.objects.filter(token=tok)
 	if(len(validate)>0):
-		lat= coorToInt(parseCoor(lat))
-		lon= coorToInt(parseCoor(lon))
+		lat= coorToInt(deparsear(lat))
+		lon= coorToInt(deparsear(lon))
 		sitios= Sitio.objects.all()
 		coors=[]
 		for i in sitios:
@@ -272,68 +268,40 @@ def sitio_close(request, lat , lon, tok):
 		return HttpResponse(sitioToJson(lista, tok))
 	else:
 		raise Http404
-		
-def parseCoor(coor):
-	lista= list(coor)
-	if lista[0]=="N":
-		lista[0]="-"
-	a= lista.index("P")
-	lista[a]="."
-	return "".join(lista)
 	
-def parseDesc(desc):
-	lista= list(desc)
-	while "_" in lista:
-		a= lista.index("_")
-		lista[a]=" "
-	return "".join(lista)
-	
-def deparse(desc):
-	lista= list(desc)
-	while " " in lista:
-		a= lista.index(" ")
-		lista[a]="_"
-	return "".join(lista)
-	
-def parseURL(url):
-	resultado=""
-	lista= list(url)
-	est= False
-	for i in lista:
-		if est==True:
-			resultado+=i.upper()
-			est=False
-		elif i=="D":
-			resultado+=":"
-		elif i=="S":
-			resultado+="/"
-		elif i=="P":
-			resultado+="."
-		elif i=="R":
-			resultado+="_"
-		elif i=="G":
-			resultado+="-"
-		elif i=="C":
-			resultado+=","
-		elif i=="A":
-			resultado+="&"
-		elif i=="V":
-			resultado+="%"
-		elif i=="E":
-			resultado+="="
-		elif i=="I":
-			resultado+="?"
-		elif i=="K":
-			resultado+="@"
-		elif i=="U":
-			resultado+="!"
-		elif i=="W":
-			resultado+="#"
-		elif i=="_":
-			est=True
-		else:
-			resultado+=i
-	return resultado
+def parsear(s):
+    dict_1= {":":"D", "/":"S",".":"P","_":"R","-":"G",",":"C","&":"A","%":"V","=":"E","?":"I","@":"K",
+         "!":"U","#":"W","¡":"B","¿":"F","<":"H",">":"J","[":"L","]":"M","(":"N",")":"O","\n":"Q",
+         "\"":"T"," ":"X",";":"Y", "$":"Z"}
+    lista= list(s)
+    resultado=""
+    for i in lista:
+        if(i in dict_1):
+            resultado+=dict_1[i]
+        elif(i.islower() or i.isdigit()):
+            resultado+=i
+        elif(i.isupper()):
+            resultado+="_"+i.lower()
+    return resultado
+
+def deparsear(s):
+    dict_1= {"D":":", "S":"/","P":".","R":"_","G":"-","C":",","A":"&","V":"%","E":"=","I":"?","K":"@",
+         "U":"!","W":"#","B":"¡","F":"¿","H":"<","J":">","L":"[","M":"]","N":"(","O":")","Q":"\n",
+         "T":"\"","X":" ","Y":";", "Z":"$"}
+    lista= list(s)
+    resultado=""
+    est= True
+    for i in lista:
+        if(est==False):
+            resultado+=i.upper()
+            est= True
+        elif(i in dict_1):
+            resultado+=dict_1[i]
+        elif(i.islower() or i.isdigit()):
+            resultado+=i
+        elif(i == "_"):
+            est= False
+    return resultado
 	
 def coorToInt(coor):
 	lista= list(coor)
@@ -371,9 +339,9 @@ def seguidores(request, tok):
 		personas= "{ personas : ["
 		for i in range(len(lista)):
 			if(i!=len(lista)-1):
-				personas+="{ id : "+str(lista[i].seguidor.id)+" , nombre : "+lista[i].seguidor.nombre+" , apellido : "+lista[i].seguidor.apellido+" , imagen : "+lista[i].seguidor.imagen+" },"
+				personas+="{ id : "+str(lista[i].seguidor.id)+" , nombre : "+parsear(lista[i].seguidor.nombre)+" , apellido : "+parsear(lista[i].seguidor.apellido)+" , imagen : "+parsear(lista[i].seguidor.imagen)+" },"
 			else:
-				personas+="{ id : "+str(lista[i].seguidor.id)+" , nombre : "+lista[i].seguidor.nombre+" , apellido : "+lista[i].seguidor.apellido+" , imagen : "+lista[i].seguidor.imagen+" }"
+				personas+="{ id : "+str(lista[i].seguidor.id)+" , nombre : "+parsear(lista[i].seguidor.nombre)+" , apellido : "+parsear(lista[i].seguidor.apellido)+" , imagen : "+parsear(lista[i].seguidor.imagen)+" }"
 		personas+="] , token :"+validate[0].token+" }"
 		return HttpResponse(personas)
 	else:
@@ -389,9 +357,9 @@ def seguidos(request, tok):
 		personas= "{ personas : ["
 		for i in range(len(lista)):
 			if(i!=len(lista)-1):
-				personas+="{ id : "+str(lista[i].seguido.id)+" , nombre : "+lista[i].seguido.nombre+" , apellido : "+lista[i].seguido.apellido+" , imagen : "+lista[i].seguido.imagen+" },"
+				personas+="{ id : "+str(lista[i].seguido.id)+" , nombre : "+parsear(lista[i].seguido.nombre)+" , apellido : "+parsear(lista[i].seguido.apellido)+" , imagen : "+parsear(lista[i].seguido.imagen)+" },"
 			else:
-				personas+="{ id : "+str(lista[i].seguido.id)+" , nombre : "+lista[i].seguido.nombre+" , apellido : "+lista[i].seguido.apellido+" , imagen : "+lista[i].seguido.imagen+" }"
+				personas+="{ id : "+str(lista[i].seguido.id)+" , nombre : "+parsear(lista[i].seguido.nombre)+" , apellido : "+parsear(lista[i].seguido.apellido)+" , imagen : "+parsear(lista[i].seguido.imagen)+" }"
 		personas+="] , token :"+validate[0].token+" }"
 		return HttpResponse(personas)
 	else:
